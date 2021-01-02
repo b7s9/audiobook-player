24cf012e271784eecca4e57846fad53e7444783e
b7s9 <interactive@b7s9.com>
2021-01-02 01:18:10 -0500

    ✨ Add minimum viable playback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-// import Howl from './components/howl';
 import Player from './components/player';
 
 function App() {
   return (
     <div className="container mx-auto p-4 bg-gray-100 dark:bg-gray-800">
-      <h1 className="font-serif my-8 text-center font-bold text-4xl text-gray-800 dark:text-gray-100">Go to Sleep</h1>
-      {/* <Howl></Howl> */}
+      <h1 className="font-serif my-6 text-right font-bold text-xl text-gray-800 dark:text-gray-100">Go to Sleep 💚</h1>
       <Player></Player>
     </div>
   );
--- src/components/player.js
+++ src/components/player.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player'
+import Duration from './duration'
+
 class Player extends Component {
 	state = {
 		url: null,
 		pip: false,
-		playing: false,
+		playing: true,
 		controls: false,
 		light: false,
 		volume: 0.8,
@@ -13,7 +15,13 @@ class Player extends Component {
 		loaded: 0,
 		duration: 0,
 		playbackRate: 1.0,
-		loop: false
+		loop: false,
+		devControls: false,
+		chapter: 0
+	}
+
+	handleToggleDevControls = () => {
+		this.setState({ devControls: !this.state.devControls })
 	}
 
 	load = url => {
@@ -116,10 +124,12 @@ class Player extends Component {
 		this.setState({ duration })
 	}
 
-	renderLoadButton = (url, label) => {
+	renderLoadButton = (url, chapter, label) => {
 		return (
-			<button onClick={() => this.load(url)}>
-				{label}
+			<button
+				className='mt-2 mr-2 px-4 py-2 block rounded shadow text-white bg-green-600 active:bg-green-700'
+				onClick={() => this.load(url)}>
+				{chapter + '. ' + label}
 			</button>
 		)
 	}
@@ -129,61 +139,209 @@ class Player extends Component {
 	}
 
 	render() {
-		const { url, playing, controls, light, volume, muted, loop, played, loaded, duration, playbackRate, pip } = this.state
-		const SEPARATOR = ' · ';
-
-		<div className='player-wrapper'>
-			<ReactPlayer
-				ref={this.ref}
-				className='react-player'
-				width='100%'
-				height='100%'
-				url={url}
-				pip={pip}
-				playing={playing}
-				controls={controls}
-				light={light}
-				loop={loop}
-				playbackRate={playbackRate}
-				volume={volume}
-				muted={muted}
-				onReady={() => console.log('onReady')}
-				onStart={() => console.log('onStart')}
-				onPlay={this.handlePlay}
-				onEnablePIP={this.handleEnablePIP}
-				onDisablePIP={this.handleDisablePIP}
-				onPause={this.handlePause}
-				onBuffer={() => console.log('onBuffer')}
-				onSeek={e => console.log('onSeek', e)}
-				onEnded={this.handleEnded}
-				onError={e => console.log('onError', e)}
-				onProgress={this.handleProgress}
-				onDuration={this.handleDuration}
-			/>
-		</div>
+		const { url, playing, controls, light, volume, muted, loop, played, loaded, duration, playbackRate, pip } = this.state // used for dev controls
 
 		return (
-			<div className="player">
-				<h2 className="font-bold">Name of Book</h2>
-				<h3><span>1. </span>Name of Chapter</h3>
-				<p>{(this.state.loaded) ? 'Loaded' : 'Loading'}</p>
-				<div className="container my-4 justify-between bg-gray-200 rounded">
-					{/* <button onClick={this.setState({ url: 'https://b7s9-static.nyc3.digitaloceanspaces.com/01-The-Boy-Who-Lived.mp3' })} className="p-4 bg-pink-300 rounded font-bold text-black">Load Chapter 1</button> */}
-					{this.renderLoadButton('https://b7s9-static.nyc3.digitaloceanspaces.com/01-The-Boy-Who-Lived.mp3', 'Chapter 1')}
-					<button onClick={this.handlePlayPause} className="p-4 bg-green-700 rounded font-bold text-white">{(this.state.playing) ? 'Pause' : 'Play'}</button>
-				</div>
-				<div className="container my-4 place-content-between bg-gray-200 rounded">
-					{/* <button onClick={this.handleSeekingChange(-15)} id="rew" className="p-4 bg-white rounded font-bold">Rewind -15 sec.</button> */}
-					{/* <button onClick={this.handleSeekingChange(15)} id="fwd" className="p-4 bg-white rounded font-bold">Forward +15 sec.</button> */}
-				</div>
 
-				{/* <ReactPlayer
-					url={'https://b7s9-static.nyc3.digitaloceanspaces.com/01-The-Boy-Who-Lived.mp3'}
-					controls={true}
-					playing={false}
-					ref={this.ref}
-				></ReactPlayer> */}
+			<div className='app'>
 
+				<section className='section'>
+					<h2 className="font-serif font-bold text-4xl">Harry Potter &amp; the Philosopher's Stone</h2>
+					<div className='chapter-select py-4' >
+						{this.renderLoadButton('audio/book-1/01-The-Boy-Who-Lived.mp3', 1, 'The Boy Who Lived')}
+						{this.renderLoadButton('https://b7s9-static.nyc3.digitaloceanspaces.com/audio-book/02-The-Vanishing-Glass.mp3', 2, 'The Vanishing Glass')}
+						{this.renderLoadButton('https://b7s9-static.nyc3.digitaloceanspaces.com/audio-book/03-The-Letters-From-No-One.mp3', 3, 'The Letters From No One')}
+						{this.renderLoadButton('https://b7s9-static.nyc3.digitaloceanspaces.com/audio-book/04-The-Keeper-Of-The-Keys.mp3', 4, 'The Keeper of the Keys')}
+					</div>
+					<h3 className='text-md'>No chapter selected</h3>
+					<div className='controls'>
+						<button
+							className='px-4 py-2 bg-blue-700 font-bold text-white text-lg rounded shadow hover:bg-blue-800'
+							onClick={this.handlePlayPause}
+						>
+							{playing ? 'Pause' : 'Play'}
+						</button>
+					</div>
+					<div className='player-wrapper'>
+						<ReactPlayer
+							ref={this.ref}
+							className='react-player'
+							width='100%'
+							height='100%'
+							url={url}
+							pip={pip}
+							playing={playing}
+							controls={controls}
+							light={light}
+							loop={loop}
+							playbackRate={playbackRate}
+							volume={volume}
+							muted={muted}
+							onReady={() => console.log('onReady')}
+							onStart={() => console.log('onStart')}
+							onPlay={this.handlePlay}
+							onEnablePIP={this.handleEnablePIP}
+							onDisablePIP={this.handleDisablePIP}
+							onPause={this.handlePause}
+							onBuffer={() => console.log('onBuffer')}
+							onSeek={e => console.log('onSeek', e)}
+							onEnded={this.handleEnded}
+							onError={e => console.log('onError', e)}
+							onProgress={this.handleProgress}
+							onDuration={this.handleDuration}
+						/>
+					</div>
+
+					<button
+						className="mt-8 px-4 py-2 bg-gray-100 font-bold rounded shadow"
+						onClick={this.handleToggleDevControls}
+					>{this.state.devControls ? 'Hide' : 'Show'} Dev Controls</button>
+
+				</section>
+				<div style={{ display: this.state.devControls ? 'block' : 'none' }}>
+					<table>
+						<tbody>
+							<tr>
+								<th>Controls</th>
+								<td>
+									<button onClick={this.handleStop}>Stop</button>
+									<button onClick={this.handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
+									{light &&
+										<button onClick={() => this.player.showPreview()}>Show preview</button>}
+									{ReactPlayer.canEnablePIP(url) &&
+										<button onClick={this.handleTogglePIP}>{pip ? 'Disable PiP' : 'Enable PiP'}</button>}
+								</td>
+							</tr>
+							<tr>
+								<th>Speed</th>
+								<td>
+									<button onClick={this.handleSetPlaybackRate} value={1}>1x</button>
+									<button onClick={this.handleSetPlaybackRate} value={1.5}>1.5x</button>
+									<button onClick={this.handleSetPlaybackRate} value={2}>2x</button>
+								</td>
+							</tr>
+							<tr>
+								<th>Seek</th>
+								<td>
+									<input
+										type='range' min={0} max={0.999999} step='any'
+										value={played}
+										onMouseDown={this.handleSeekMouseDown}
+										onChange={this.handleSeekChange}
+										onMouseUp={this.handleSeekMouseUp}
+									/>
+								</td>
+							</tr>
+							<tr>
+								<th>Volume</th>
+								<td>
+									<input type='range' min={0} max={1} step='any' value={volume} onChange={this.handleVolumeChange} />
+								</td>
+							</tr>
+							<tr>
+								<th>
+									<label htmlFor='controls'>Controls</label>
+								</th>
+								<td>
+									<input id='controls' type='checkbox' checked={controls} onChange={this.handleToggleControls} />
+									<em>&nbsp; Requires player reload</em>
+								</td>
+							</tr>
+							<tr>
+								<th>
+									<label htmlFor='muted'>Muted</label>
+								</th>
+								<td>
+									<input id='muted' type='checkbox' checked={muted} onChange={this.handleToggleMuted} />
+								</td>
+							</tr>
+							<tr>
+								<th>
+									<label htmlFor='loop'>Loop</label>
+								</th>
+								<td>
+									<input id='loop' type='checkbox' checked={loop} onChange={this.handleToggleLoop} />
+								</td>
+							</tr>
+							<tr>
+								<th>
+									<label htmlFor='light'>Light mode</label>
+								</th>
+								<td>
+									<input id='light' type='checkbox' checked={light} onChange={this.handleToggleLight} />
+								</td>
+							</tr>
+							<tr>
+								<th>Played</th>
+								<td><progress max={1} value={played} /></td>
+							</tr>
+							<tr>
+								<th>Loaded</th>
+								<td><progress max={1} value={loaded} /></td>
+							</tr>
+						</tbody>
+					</table>
+					<section className='section'>
+						<table>
+							<tbody>
+								<tr>
+									<th>Files</th>
+									<td>
+
+									</td>
+								</tr>
+								<tr>
+									<th>Custom URL</th>
+									<td>
+										<input ref={input => { this.urlInput = input }} type='text' placeholder='Enter URL' />
+										<button onClick={() => this.setState({ url: this.urlInput.value })}>Load</button>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+
+						<h2>State</h2>
+
+						<table>
+							<tbody>
+								<tr>
+									<th>url</th>
+									<td className={!url ? 'faded' : ''}>
+										{(url instanceof Array ? 'Multiple' : url) || 'null'}
+									</td>
+								</tr>
+								<tr>
+									<th>playing</th>
+									<td>{playing ? 'true' : 'false'}</td>
+								</tr>
+								<tr>
+									<th>volume</th>
+									<td>{volume.toFixed(3)}</td>
+								</tr>
+								<tr>
+									<th>played</th>
+									<td>{played.toFixed(3)}</td>
+								</tr>
+								<tr>
+									<th>loaded</th>
+									<td>{loaded.toFixed(3)}</td>
+								</tr>
+								<tr>
+									<th>duration</th>
+									<td><Duration seconds={duration} /></td>
+								</tr>
+								<tr>
+									<th>elapsed</th>
+									<td><Duration seconds={duration * played} /></td>
+								</tr>
+								<tr>
+									<th>remaining</th>
+									<td><Duration seconds={duration * (1 - played)} /></td>
+								</tr>
+							</tbody>
+						</table>
+					</section>
+				</div>
 			</div>
 		)
 	}
