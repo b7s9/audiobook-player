6ed09b3f9f7385308b71f47a8577addaaa9771fc
b7s9 <interactive@b7s9.com>
2021-01-26 21:33:48 -0500

    💄 Update dropdown colors

--- src/components/chapter-load-button.js
+++ src/components/chapter-load-button.js
@@ -5,7 +5,7 @@ class ChapterLoadButton extends Component {
         return (
             <option
                 key={this.props.index}
-                className="w-full box-border mt-2 mr-2 px-4 py-2 block rounded shadow text-white bg-gray-50 hover:bg-gray-300 dark:bg-gray-900 dark:hover:bg-gray-700"
+                className="w-full box-border mt-2 mr-2 px-4 py-2 block rounded shadow bg-gray-200 text-gray-900 dark:bg-gray-800 dark:text-gray-400"
                 value={this.props.index}
             >
                 {this.props.index + '. ' + this.props.title}
